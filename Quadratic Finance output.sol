dddaf43a5962ac55bd12215e2b1a6ca373fb700e
CaravanRussell <87099926+CaravanRussell@users.noreply.github.com>
2021-07-26 21:34:26 -0400

    Add files via upload

---
+++ Quadratic Finance output.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.8.4;
+
+contract QuadraticFinance {
+    mapping (address => bool) donor;
+    uint256 public sqrt_total;
+    
+    receive() external payable {
+        require(donor[msg.sender] == false,
+            "You have already donated");
+        uint256 sqrt_amount=sqrt_uniswap(msg.value);
+        sqrt_total=sqrt_total+sqrt_amount;
+        donor[msg.sender]=true;
+    }
+    
+    fallback() external payable {}
+    
+    // babylonian method (https://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Babylonian_method)
+     function sqrt_uniswap(uint y) public pure returns (uint z) {
+        if (y > 3) {
+            z = y;
+            uint x = y / 2 + 1;
+            while (x < z) {
+                z = x;
+                x = (y / x + x) / 2;
+            }
+        } else if (y != 0) {
+            z = 1;
+        }
+    }
+    
+    function total_contribution() public view returns (uint256) {
+        return (sqrt_total) ** 2;
+    }
+}
